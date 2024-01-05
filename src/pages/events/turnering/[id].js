e38a3e8b065c8614b363eb63148209202dff411f
Jachob Westh <jachobwesth@gmail.com>
2024-01-05 08:51:20 +0100

    here you go

--- src/pages/events/turnering/[id].js
+++ src/pages/events/turnering/[id].js
@@ -7,6 +7,8 @@ import { Layout } from '@/Layout';
 import { format } from 'date-fns';
 import { FaCalendar, FaCoins, FaGamepad, FaMoneyBill } from 'react-icons/fa';
 import { BsFillPeopleFill } from 'react-icons/bs';
+import { Link } from '@radix-ui/react-navigation-menu';
+import Countdown from 'react-countdown';
 
 function page() {
   const router = useRouter();
@@ -42,6 +44,19 @@ function page() {
 
   const dateObject = new Date(timestamp);
 
+  const countdownDate = Date.now() + Date.parse(timestamp);
+
+  const renderer = ({ hours, minutes, seconds, days, completed }) => {
+    return (
+      <span>
+        {days} dage {hours}:{minutes}:{seconds}
+      </span>
+    );
+  };
+
+  console.log('checker', Date.now());
+  console.log('checker 2', Date.parse(timestamp));
+
   console.log(format(dateObject, 'dd/MM HH:mm'));
 
   return (
@@ -58,17 +73,31 @@ function page() {
               <div className='w-full spacer max-w-main flex flex-col justify-end'>
                 <h1>{data.eventNavn}</h1>
                 <h2>{data.spil}</h2>
-                <div className='flex gap-4 align-middle'>
-                  <div className='bg-accentCol w-fit px-2 self-center rounded-full flex'>
-                    <p className='text-primaryCol mt-0 font-medium'>OPEN</p>
+                <div className='flex justify-between items-center'>
+                  <div className='flex gap-4 align-middle'>
+                    <div className='bg-accentCol w-fit px-2 self-center rounded-full flex'>
+                      <p className='text-primaryCol mt-0 font-medium'>OPEN</p>
+                    </div>
+                    <h4 className='mt-0'>
+                      Tilmeldingsfrist D. {format(dateObject, 'dd/MM HH:mm')}
+                    </h4>
                   </div>
-                  <h4 className='mt-0'>Tilmeldingsfrist D. {format(dateObject, 'dd/MM HH:mm')}</h4>
+                  <div className='flex flex-col gap-2 justify-center'>
+                    <p className='text-center text-2xl'>
+                      <Countdown date={Date.parse(timestamp)} />
+                    </p>
+                    <a href='/om-os/kontakt?turnering'>
+                      <div className='w-fit px-8 py-2 font-bold uppercase rounded-sm h-min bg-accentCol'>
+                        Tilmeld
+                      </div>
+                    </a>
+                  </div>
                 </div>
               </div>
             </header>
             <section className='flex justify-center'>
               <div className='spacer w-full flex-wrap flex justify-between'>
-                <div className='shrink-0 '>
+                <div className='grow'>
                   <div
                     style={{
                       gridTemplateColumns: 'repeat(auto-fill, minmax(200px, 1fr))',
@@ -102,9 +131,12 @@ function page() {
                       <p className='mt-0'>{data.gebyr}kr.</p>
                     </div>
                   </div>
-                  <p>{data.beskrivelse}</p>
+                  <div>
+                    <p className='text-xl uppercase font-bold'>{data.subheader}</p>
+                    <p>{data.beskrivelse}</p>
+                  </div>
                 </div>
-                <div></div>
+
                 <div>
                   <h4>Sponsorer</h4>
                 </div>
